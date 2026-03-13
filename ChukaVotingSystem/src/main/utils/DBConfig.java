24e8d33b8d21d4573e67c512b53c9fa51b37d8ed
Barrack.k <Barrackkainga@gmail.com>
2026-03-13 14:14:18 +0300

    updated database logins

--- ChukaVotingSystem/src/main/utils/DBConfig.java
+++ ChukaVotingSystem/src/main/utils/DBConfig.java
@@ -1,10 +1,10 @@
 package main.utils;
 
 public class DBConfig {
-    public static final String HOST     = "localhost";
-    public static final String PORT     = "3306";
-    public static final String DATABASE = "chuka_voting_db";
-    public static final String USERNAME = "root";
+    public static final String HOST     = "localhost";// Updated host
+    public static final String PORT     = "3306";// Updated port number
+    public static final String DATABASE = "chuka_voting_db";// Updated database name
+    public static final String USERNAME = "root";// Updated username
     public static final String PASSWORD = "Monkey"; // Updated password
 
     public static final String URL =
