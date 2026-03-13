a8c1da279373f2cf48508e8773fb11d913dae16d
Barrack.k <Barrackkainga@gmail.com>
2026-03-13 15:13:21 +0300

    database login update

--- ChukaVotingSystem/src/main/utils/DBConfig.java
+++ ChukaVotingSystem/src/main/utils/DBConfig.java
@@ -1,5 +1,5 @@
 package main.utils;//package carrying DBconfigurations creditials
-//tests
+// This class contains the database configuration details such as host, port, database name, username, and password. It also constructs the JDBC URL for connecting to the MySQL database.
 public class DBConfig {
     public static final String HOST     = "localhost";// Updated host
     public static final String PORT     = "3306";// Updated port number
